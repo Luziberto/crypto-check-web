104ff0e7b4d709db925dd877587fd16143d72603
Luziberto Mendes <luzibertomendesjunior@hotmail.com>
2022-11-01 11:17:06 -0300

    setup project dev dependecies

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { fileURLToPath, URL } from 'url'
+import eslintPlugin from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: process.env.NODE_ENV === 'production' ? '/asset-web/' : '/',
-  plugins: [vue()],
+  plugins: [vue(), eslintPlugin()],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url))
